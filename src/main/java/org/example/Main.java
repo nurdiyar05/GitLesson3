2211182dcc8a56fd711723331e5a29d2a25f0ce3
nurdiyar05 <nnurkyran@gmail.com>
2023-06-29 15:38:25 +0600

    Created new project for able git workshop

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Main {
+    public static void main (String[]args) {
+        mainMethodShowMessageable();
+        mainMethodShowInfo();
+    }
+   public static void  mainMethodShowMessageable () {
+       System.out.println("Hello able QA course\n");
+   }
+       public static void mainMethodShowInfo (){
+       System.out.println("new changes");
+
+    }
+}
